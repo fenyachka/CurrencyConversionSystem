f72edffae20d5564ffdc61d7739e089aad4ff080
fenya <fenyamuradova@gmail.com>
2022-06-13 18:07:26 +0400

    configs

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,16 +8,26 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using API.Middleware;
+using Application.Core;
+using Domain.Interfaces;
+using FluentValidation.AspNetCore;
+using Infrastructure.Config;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
         public IConfiguration Configuration { get; }
@@ -25,17 +35,34 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.Configure<AmountConfiguration>(_config.GetSection("AmountConfiguration"));
+            
+            
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+            });
+        
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IRepository<>), (typeof(Repository<>)));
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API",
+                    Version = "v1",
+                    Description = "Currency Conversion System"
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ Application/Core/AmountConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Core
+{
+    public class AmountConfiguration
+    {
+        public decimal DailyAmountLimit { get; set; }
+    }
+}
